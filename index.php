afe27a321274b6f24692b56ec9c5647695f56e9f
Willemu <108131589+Willemu@users.noreply.github.com>
2023-03-08 10:58:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -92,9 +92,7 @@
 			<br>
 			<br>
 		<input type="submit" value="Submit">
-
-		<img src="images/chocolatedonut.png" alt="chocolate donut" width="200" height="200" class="donutpic1">
-		<br>
+		
 	</form>
 			
        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>	
